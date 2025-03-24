8c6d0204db39876562494111c61d35433a74813c
tanaya <ttanyasharma123@gmail.com>
2025-03-24 11:40:02 +0530

    [Tanya]: Add UC3 implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 
-@Component({
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'AngularProject';
+ })
+ export class AppComponent {
+   title = 'AngularProject';
   imgUrl= "/assets/BridgeLabz.jpg";
+  url = "C:\Users\tanya\OneDrive\Desktop\BridgeLabz.jpg";
 
-  ngOnInit(): void {
+ ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
 
   }
-}
+// }
 
+
+  onClick($event){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url, "_blank");
+
+  }
+}
